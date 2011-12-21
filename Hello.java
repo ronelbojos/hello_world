ff70788b2490f8ef3e1385f9ad583be43e05e633
Ronel Bojos <ronelbojos@gmail.com>
2011-12-21 20:39:31 +0800

    added JOptionPane

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
+import javax.swing.JOptionPane;
 public class Hello {
   public static void main (String [] args) {
     System.out.println("Hello World!");
+    String name = JOptionPane.showInputDialog("What's your name?","Ron");
+    JOptionPane.showMessageDialog(null,"Hello " + name + ", nice to meet you!");
   }
 }
